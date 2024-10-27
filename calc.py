aa4f7a0ff5de0a3eed44a26a3814499b7c0b48d3
Alex <a.dziendziol@jarvia.de>
2024-10-27 23:47:53 +0100

    commit

---
+++ calc.py
@@ -0,0 +1,91 @@
+from items import piItem,piItems
+from collections import defaultdict
+from copy import copy
+
+import json
+
+def calcAmount(itemName,quantity):
+    array = []
+    quantity = checkAndAdjustMinimalQuantity(itemName,quantity)
+    
+    items = [piItem for piItem in piItems if piItem.name == itemName]
+
+    # find first level
+    for item in items:
+            if item not in array:
+                item.inputQuantity = item.inputQuantity * round(item.total /   item.outputQuantity)  
+                item.outputQuantity = quantity
+                array.append(copy(item))
+    # find all other levels
+    findLevelsWithValues(items,array)
+
+    return array
+
+def checkAndAdjustMinimalQuantity(itemName,quantity):
+    for piItem in piItems:
+            if piItem.name==itemName:
+                minimalQuantity = piItem.outputQuantity
+                break
+
+    if quantity % minimalQuantity > 0:
+          quantity = quantity + (minimalQuantity - quantity % minimalQuantity)
+    return quantity
+
+
+def findLevelsWithValues(resultList,array): 
+    for resultItem in resultList:
+        items = [piItem for piItem in piItems if piItem.name == resultItem.inputName]
+        for item in items:
+            item.inputQuantity = item.inputQuantity * round(resultItem.inputQuantity /   item.outputQuantity)  
+            item.outputQuantity = resultItem.inputQuantity
+            array.append(copy(item))
+        # recursion baby
+        findLevelsWithValues(items,array)
+    return array
+
+
+def merge_itemsLists(lists):
+    merged = defaultdict(lambda: {'outputQuantity': 0, 'inputQuantity': 0, 'inputNames': set()})
+    
+    for piItems in lists:
+        for item in piItems:
+            key = (item.name, item.inputName)
+            merged[key]['outputQuantity'] += item.outputQuantity
+            merged[key]['inputQuantity'] += item.inputQuantity
+            merged[key]['inputNames'].add(item.inputName)
+    
+    final_merge = {}
+    for (name, inputName), values in merged.items():
+        if name not in final_merge:
+            final_merge[name] = {
+                'outputQuantity': values['outputQuantity'],
+                'inputQuantity': values['inputQuantity'],
+                'inputNames': values['inputNames']
+            }
+        else:
+            # Bei unterschiedlichem inputName keine Summation der Werte
+            final_merge[name]['inputNames'].update(values['inputNames'])
+    
+    result = []
+    for name, values in final_merge.items():
+        if len(values['inputNames']) > 1:
+            input_name = ""
+        else:
+            input_name = next(iter(values['inputNames']))
+        result.append({
+            'name': name,
+            'inputName': input_name,
+            'outputQuantity': values['outputQuantity'],
+            'inputQuantity': values['inputQuantity']
+        })
+    
+    return result
+
+prodList1 = calcAmount('Vaccines',6)
+prodList2 = calcAmount('Neocoms',1)
+
+
+
+merged_items = merge_itemsLists([prodList1, prodList2])
+for item in merged_items:
+    print(item)
